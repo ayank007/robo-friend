43e1e4b47715a8c293d9790171deffc692e55e12
unknown <ayankoley@gmail.com>
2020-08-26 21:32:24 +0530

    Updates

---
+++ precache-manifest.14162067f2b576bc5904dee212d39af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b179390a50611845f17bb7995822c9f2",
+    "url": "/robo-friend/index.html"
+  },
+  {
+    "revision": "2706720fa8472e2a4ef5",
+    "url": "/robo-friend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "99f8d71d3bc706fc7c92",
+    "url": "/robo-friend/static/css/main.31cdfe4d.chunk.css"
+  },
+  {
+    "revision": "2706720fa8472e2a4ef5",
+    "url": "/robo-friend/static/js/2.a0e2e58f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robo-friend/static/js/2.a0e2e58f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f8d71d3bc706fc7c92",
+    "url": "/robo-friend/static/js/main.6065310b.chunk.js"
+  },
+  {
+    "revision": "a75c699395291118ad76",
+    "url": "/robo-friend/static/js/runtime-main.56e1bcf8.js"
+  }
+]);
